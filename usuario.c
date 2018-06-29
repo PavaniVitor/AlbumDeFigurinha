a6fe62e8ffdd39de7d3947f760d919c8528351a9
vitor pavani <vitorpavani@poli.ufrj.br>
2018-06-29 18:30:11 -0300

    adiciona o main e usuario

---
+++ usuario.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    cadastro();
+}
+void cadastro()
+{
+    //entrar nome de usuario;
+    FILE *fp;
+    char *p;
+    char usuario[20];
+    char senha[8];
+    scanf("%s",&usuario);
+    //entrar senha;
+    scanf("%s",&senha);
+    fp = fopen(usuario ,"a+");
+    while (*p)
+    {
+
+    }
+}
