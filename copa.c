c8eb0caf584d03c52dbdaf8638f2fad9d9c55e00
vitor pavani <vitorpavani@poli.ufrj.br>
2018-06-29 23:50:13 -0300

    funçao de cadastro

--- copa.c
+++ copa.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void cadastro()
+int cadastro()
 {
     FILE *fp;
-    char usuario[20];
-    char senha[8];
-    printf("Digite o nome do usuario");
-    scanf("%s" , &usuario[0]);
-    printf("Digite a senha");
-    scanf("%s", &senha[0]);
-    printf("%s",senha);
+    int count = 0;
+    char usuario[25] = {};
+    char senha[8]= {};
+    printf("Digite o nome do usuario\n");
+    scanf("%s", &usuario[0]);
 
-    fp = fopen(usuario ,"w+");
-    fclose(fp);
+    while (usuario[count]!='\0')
+    {
+        count++;
+    }
+    if(count < 20)
+    {
+        usuario[count] = '.';
+        usuario[count+1] = 't';
+        usuario[count+2] = 'x';
+        usuario[count+3] = 't';
+        fp = fopen(usuario, "r");
+
+        if (fp)
+        {
+            printf("esse usuario ja existe\n");
+            return 1;
+        }
+        else
+        {
+            fp = fopen(usuario, "w+");
+            printf("novo usuario criado\n");
+            printf("Digite a senha\n");
+            scanf("%s", &senha[0]);
+            return 0;
+        }
+        
+    }
+    else
+    {
+        printf("numero maximo de caracteres atingidos\n");
+    }
+    return 1; // retornou 1 a gente sabe que ocorreu um erro na hora de criar o usuario;
 }
 void main()
 {
-    cadastro();
+    while(cadastro())
+    {
+        cadastro();
+    }
 }
